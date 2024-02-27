b9c06f9bff934dbfe1eab48c05263b736835f710
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-02-27 11:45:38 +0800

    Add model

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const { Sequelize, DataTypes, Model } = require('sequelize')
+const express = require('express')
+const app = express()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-  }
-}
 
-main()
+class Note extends Model {}
+Note.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    important: {
+      type: DataTypes.BOOLEAN,
+    },
+    date: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'note',
+  }
+)
+
+app.get('/api/notes', async (req, res) => {
+  const notes = await Note.findAll()
+  res.json(notes)
+})
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
