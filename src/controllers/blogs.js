be072dc084c936a616d09f2c77db9f2918004aa3
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-03-06 11:11:10 +0800

    Finish 13.5

---
+++ src/controllers/blogs.js
@@ -0,0 +1,52 @@
+const router = require('express').Router()
+
+const {Blog} = require('../models')
+
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+router.get('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    console.log(req.blog.toJSON())
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+router.put('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    req.blog.important = req.body.important
+    await req.blog.save()
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    console.log(req.blog.toJSON())
+    await req.blog.destroy()
+    res.status(200).json('deleted')
+  } else {
+    res.status(404).end()
+  }
+})
+module.exports = router
