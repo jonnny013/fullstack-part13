f53512c9fbefd51a65e21a93b1f4624ab414c355
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-03-06 15:24:19 +0800

    Finish 13.13

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const {Blog, User} = require('../models')
 const {tokenExtractor} = require('../util/middleware')
+const { Op } = require('sequelize')
 
 const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id, {
@@ -14,12 +15,21 @@ const blogFinder = async (req, res, next) => {
 }
 
 router.get('/', async (req, res) => {
+  let where = {}
+
+  if (req.query.search) {
+    where.title = {
+      [Op.substring]: req.query.search
+    }
+  }
+
   const blogs = await Blog.findAll({
     attributes: { exclude: ['userId'] },
     include: {
       model: User,
       attributes: ['name'],
     },
+    where
   })
   res.json(blogs)
 })
