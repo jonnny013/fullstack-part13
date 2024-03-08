063c7fb106843be3ab82dd047d8d3b4faa64bbc7
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-03-08 11:42:06 +0800

    Finish 13.22

--- src/controllers/readinglists.js
+++ src/controllers/readinglists.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {  ReadingList } = require('../models')
+const {  ReadingList, User } = require('../models')
 const {tokenExtractor} = require('../util/middleware')
 
 
@@ -7,6 +7,17 @@ router.post('/', tokenExtractor, async (request, response) => {
   const body = request.body
   const reading_lists = await ReadingList.create(body)
   return response.json(reading_lists)
+})
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const listToUpdate = await ReadingList.findByPk(req.params.id)
+  const user = await User.findByPk(req.decodedToken.id)
+  if (user.id !== listToUpdate.userId) {
+    return res.status(403).send('Restricted access')
+  }
+  listToUpdate.unread = req.body.unread
+  const result = await listToUpdate.save()
+  res.json(result)
 })
 
 module.exports = router
